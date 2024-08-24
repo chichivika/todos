0da868118041005530466c0137791259041a6f32
chichivika <vamyrikova00@gmail.com>
2024-08-24 22:49:51 +0300

    update color name

--- src/components/base/btnGroup/styled.tsx
+++ src/components/base/btnGroup/styled.tsx
@@ -1,11 +1,12 @@
 import Button from "../button/Button";
 import styled from "styled-components";
+import colors from "styles/colors";
 
 export const StyledBtnGroup = styled.div``;
 export const StyledBtnItem = styled(Button) <{ $selected?: boolean }>`
     &.MuiButtonBase-root.MuiButton-root{
         border-radius: 0px;
         border-bottom: 1px solid transparent;
-        border-color: ${props => props.$selected ? 'grey' : 'transparent'};
+        border-color: ${props => props.$selected ? colors.baseColor : 'transparent'};
     }
 `
